214f2469cc92b94e6814042c03523318a1c3d60e
Pavel Perestoronin <eigenein@gmail.com>
2014-08-12 23:03:45 +0300

    Docstrings and logging

--- kit.py
+++ kit.py
@@ -1,31 +1,45 @@
 import json
+import logging
+import sys
 
 import click
 
 
-@click.command(help="unpack replay")
+@click.command(short_help="unpack replay")
 def unpack():
     pass
 
 
-@click.command(help="disassemble into packets")
+@click.command(short_help="disassemble into packets")
 def dis():
     pass
 
 
-@click.command(help="assemble packets")
+@click.command(short_help="assemble packets")
 def asm():
     pass
 
 
-@click.command(help="pack replay")
+@click.command(short_help="pack replay")
 def pack():
     pass
 
 
 @click.group()
-def main():
-    pass
+@click.option("-v", "--verbose", is_flag=True)
+def main(verbose):
+    """
+    World of Tanks Replay Toolkit.
+
+    Use `unpack` command followed by `dis` command to unpack replay into sequence of packets.
+
+    Use `asm` command followed by `pack` command to pack sequence of packets back into replay.
+    """
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s %(message)s",
+        level=(logging.INFO if not verbose else logging.DEBUG),
+        stream=sys.stderr,
+    )
 
 
 main.add_command(unpack)
